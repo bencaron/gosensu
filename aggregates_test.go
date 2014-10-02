1114352ece6f045cb4acf44f6495694e37e627af
Benoit Caron <bcaron@lapresse.ca>
2014-10-01 21:50:27 -0400

    Skip aggregates when testing against sensu-canned

--- aggregates_test.go
+++ aggregates_test.go
@@ -2,11 +2,15 @@ package sensu
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestGetAggregates(t *testing.T) {
 	sensu := getSensuTester()
+	if strings.Contains(sensu.URL, "localhost") {
+		t.Skip("Not testing aggregates with canned")
+	}
 	agg, err := sensu.GetAggregates(10, 0)
 	if err != nil {
 		t.Error(fmt.Sprintf("Sensu agg returned and error: %v", err))
